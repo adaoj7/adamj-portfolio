a7e4e49e5bff3e5591474318fe958f00b16636da
Adam Johnson <adaojohnson902@gmail.com>
2024-01-12 20:15:11 -0700

    installed tailwindcss and daisyui

---
+++ src/Practice.tsx
@@ -0,0 +1,8 @@
+﻿// This is where I plan on writing any practice that I do with my code and potentially to do my leetcode as well
+
+// Data structures are an important thing that I need to be working on
+
+let x=2
+for (let i = 2; i < 9; i++) {
+    x++
+}
