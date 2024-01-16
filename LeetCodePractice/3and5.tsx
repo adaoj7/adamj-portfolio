a2dcd56f6015d4831a46a4802431fb8131af9195
Adam Johnson <adaojohnson902@gmail.com>
2024-01-15 18:45:39 -0700

    working on 3and5 problem

---
+++ LeetCodePractice/3and5.tsx
@@ -0,0 +1,18 @@
+﻿//List all numbers less than the given number that are multiples of 3 or 5 if number is negative return 0
+
+// Given 25 we would get 21, 20, 18, 15, 12, 10, 9, and 6
+// Then we need the sum of these numbers
+
+// Problems I will run into 
+
+const solution = (number) => {
+    // start with the sum being 0
+    let sum = 0
+
+    // loop through all numbers less than given number and greater than 0
+    for (let i = number; i > 0; i--) {
+        // if number % 3 === 0 add number to sum
+        // if number % 5 === 0 add number to sum
+        if ()
+    }
+}
