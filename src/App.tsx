6add91de8d473a0b72b1a6b1abfdb31629d40ff0
Adam Johnson <adaojohnson902@gmail.com>
2024-01-29 17:23:20 -0700

    changed some of the basic html layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,81 +5,65 @@ import Profile from "./assets/Edited Headshot.jpeg";
 function App() {
     const [count, setCount] = useState(0);
 
-
     return (
-        <div className="flex flex-row justify-around h-full">
-            <header className=" flex justify-center w-1/2">
-                <div className="fixed top-52">
-                    <h1 className="flex text-6xl text-white whitespace-nowrap">
-                        Hi, I'm Adam Johnson
-                    </h1>
-                    {/* <p className="flex text-4xl text-white">React Developer</p> */}
-                    <div className="px-2 mt-8">
-
-                    <p className="w-[575px] text-white">
-                        I found my love for tech after already finishing my
-                        degree in Public Policy and Administration. One day, the
-                        semester before I graduated while talking with my wife,
-                        a feeling hit me really strongly.{" "}
-                        <span className="font-bold text-info">
-                            "I should learn to code."{" "}
-                        </span>
-                    </p>
-                    <p className="w-[575px] mt-4 text-white">
-                        The Monday after I graduated from university I began a
-                        coding bootcamp at Devmountain specializing in web
-                        development. I was not prepared for the joy that came
-                        into my life as a result.
-                    </p>
-                    <p className="w-[575px] mt-4 text-white">
-                        I am a new father, an avid golfer, a soccer player and
-                        coach, a hungry learner, and a <span className="font-bold text-info">React web developer.</span>
-                    </p>
-                    </div>
-                    <div>
-
-                    </div>
-                </div>
+        <div className="flex flex-col h-full">
+            <header className="flex justify-evenly text-white">
+                <section>
+                    <p className="font-bold">Adam Johnson</p>
+                    <p className="font-bold">React Web Developer</p>
+                </section>
+                <section>
+                    <p className="underline">adaojohnson902@gmail.com</p>
+                </section>
             </header>
-            <main className="flex flex-col w-1/2 h-[8000px]">
-              <div className="flex justify-center">
-
-                <div className="flex justify-center border-[15px] w-[650px] mt-20 border-black rounded-xl">
-                    <img className="" src={Profile} />
-                </div>
-              </div>
-                <div className="flex justify-center h-2/3">
-                    <button
-                        onClick={() => setCount((count) => count + 2)}
-                        className="flex btn btn-info w-80 m-6 rounded-full"
-                    >
-                        count is {count}
-                    </button>
-                </div>
-                <form>
-                    <fieldset className="form-control w-80">
-                        <label className="label">
-                            <span className="label-text">
-                                Enter your email address
+            <main className="h-[8000px]">
+                <h1 className="flex justify-center font-bold text-4xl text-white">
+                    Hi, I'm Adam
+                </h1>
+                 <div className="flex flex-row justify-around">
+                    <section className="w-1/3">
+                        {/* <p className="flex text-4xl text-white">React Developer</p> */}
+                        <div className="text-lg">
+                            <p className="text-white">
+                                I found my love for tech after already finishing
+                                my Bachelor's degree in Public Policy and
+                                Administration. One day, the semester before I
+                                graduated while talking with my wife, a feeling
+                                hit me really strongly.{" "}
+                                <span className="font-bold text-info">
+                                    "I should learn to code."{" "}
+                                </span>
+                            </p>
+                            <p className="text-white">
+                                The Monday after I graduated from university I
+                                began a coding bootcamp at Devmountain
+                                specializing in web development. I was not
+                                prepared for the joy that came into my life as a
+                                result.
+                            </p>
+                            <p className=" text-white text-nowrap">
+                                I'm a new father, an avid golfer, a soccer
+                                player and coach, a hungry learner, and a{" "}
+                            </p>
+                            <span className="font-bold text-info">
+                                React Web Developer
                             </span>
-                        </label>
-                        <div className="join">
-                            <input
-                                type="text"
-                                placeholder="username@site.com"
-                                className="input input-bordered join-item"
-                            />
-                            <button
-                                className="btn btn-info rounded-full join-item"
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                }}
-                            >
-                                Subscribe
-                            </button>
+                        <div>
+                            <a href="https://www.linkedin.com/in/adamj7/">
+                                LinkedIn
+                            </a>
+                            <a href="https://github.com/adaoj7">GitHub</a>
                         </div>
-                    </fieldset>
-                </form>
+                        </div>
+                    </section>
+                    <section>
+                        <div className="">
+                            <div className="border-[15px] w-[350px] border-black rounded-xl">
+                                <img className="" src={Profile} />
+                            </div>
+                        </div>
+                    </section>
+                </div>
             </main>
         </div>
     );
