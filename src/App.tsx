c9d3c5b3b0ab9a6b9823d8c7f8ca2e64187b5383
Adam Johnson <adaojohnson902@gmail.com>
2024-01-29 19:42:53 -0700

    switched sides of the picture and text and added a mailto: button in header

--- src/App.tsx
+++ src/App.tsx
@@ -12,45 +12,12 @@ function App() {
                     <p className="text-sm">React Web Developer</p>
                 </section>
                 <section className="flex flex-row justify-center">
-                    <p className="flex items-center underline">
-                        adaojohnson902@gmail.com
-                    </p>
+                 <button onClick={() => window.location.href = 'mailto:adaojohnson902@gmail.com'} className="flex items-center hover:text-gray-300">Contact Me</button>
                 </section>
             </header>
             <main className="h-[2000px]">
                 <section className="flex flex-row justify-evenly h-1/4 mt-20">
-                    <div className="flex w-1/3 justify-center items-center">
-                        <div className=" text-lg">
-                            <h2 className="text-white text-4xl font-Playfair mb-6">
-                                Hi, I'm Adam
-                            </h2>
-                            <p className="text-white">
-                                I found my love for tech after already finishing
-                                my Bachelor's degree in Public Policy and
-                                Administration. One day, the semester before I
-                                graduated while talking with my wife, a feeling
-                                hit me really strongly.{" "}
-                                <span className="font-bold text-info">
-                                    "I should learn to code."{" "}
-                                </span>
-                            </p>
-                            <p className="text-white">
-                                The Monday after I got my degree I began a
-                                coding bootcamp at Devmountain specializing in
-                                web development. I was not prepared for the joy
-                                that came into my life as a result.
-                            </p>
-                            <p className=" text-white text-nowrap">
-                                I'm a new father, an avid golfer, a soccer
-                                player and coach, a hungry learner, and a{" "}
-                            </p>
-                            <span className="font-bold text-info">
-                                React Web Developer
-                            </span>
-                            <div className="flex flex-row justify-center mt-20"></div>
-                        </div>
-                    </div>
-                    <section className="flex items-center justify-start ">
+                <section className="flex items-center justify-start ">
                         <div>
                             <div className="border-[15px] w-[350px] border-black rounded-xl">
                                 <img className="" src={Profile} />
@@ -68,6 +35,38 @@ function App() {
                             </div>
                         </div>
                     </section>
+                    <div className="flex w-1/3 justify-center items-center">
+                        <div className=" text-lg">
+                            <h2 className="text-white text-4xl font-Playfair mb-6">
+                                Hi, I'm Adam
+                            </h2>
+                            <p className="text-white">
+                                I found my love for tech after already finishing
+                                my Bachelor's degree in Public Policy and
+                                Administration. One day, the semester before I
+                                graduated while talking with my wife, a feeling
+                                hit me really strongly.{" "}
+                                <span className="font-bold text-info">
+                                    "I should learn to code."{" "}
+                                </span>
+                            </p>
+                            <p className="mt-3 text-white">
+                                The Monday after I got my degree I began a
+                                coding bootcamp at Devmountain specializing in
+                                web development. I was not prepared for the joy
+                                that came into my life as a result.
+                            </p>
+                            <p className="mt-3 text-white text-nowrap">
+                                I'm a new father, an avid golfer, a soccer
+                                player and coach, a hungry learner, and a{" "}
+                            </p>
+                            <span className="font-bold text-info">
+                                React Web Developer
+                            </span>
+                            <div className="flex flex-row justify-center mt-20"></div>
+                        </div>
+                    </div>
+                    
                 </section>
             </main>
             <footer></footer>
