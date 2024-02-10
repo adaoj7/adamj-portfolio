d7f9a34561c5cf88028fe3680fa49dc8a88cee96
Adam Johnson <adaojohnson902@gmail.com>
2024-02-09 20:37:33 -0700

    changed hero styling and added fixed button to bottom right

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,18 @@ import Profile from "./assets/Edited Headshot.jpeg";
 import Github from "./assets/github-mark-white.png";
 import LinkedIn from "./assets/In-White-128.png";
 import NatsiteHero from "./assets/Natsite.png";
-import MailIcon from "./assets/Mail.svg"
-import AmaviHero from './assets/AmaviChorale.png'
-import MovieHero from './assets/Movie Lookup.png'
+import MailIcon from "./assets/Mail.svg";
+import AmaviHero from "./assets/AmaviChorale.png";
+import MovieHero from "./assets/Movie Lookup.png";
 
 function App() {
     return (
         <div className="flex flex-col h-full font-OpenSans">
-            <header className="flex justify-around text-white h-24 bg-primary-content">
-                <section className="flex flex-col justify-center ">
-                    <p className="font-bold">Adam Johnson</p>
-                    <p className="text-sm">React Web Developer</p>
-                </section>
+            <header className="relative">
                 {/* Move modal button to bottom corner on scroll */}
-                <section className="flex flex-row justify-center items-center">
+                <section className="fixed bottom-12 right-12">
                     <button
-                        className="hover:text-gray-400"
+                        className="btn btn-info hover:btn-ghost shadow-xl"
                         onClick={() =>
                             // @ts-ignore
                             document.getElementById("my_modal_1").showModal()
@@ -28,8 +24,8 @@ function App() {
                     </button>
                     <dialog id="my_modal_1" className="modal">
                         <div className="modal-box">
-                            <h3 className="font-bold text-lg">Hello!</h3>
-                            <p className="py-4">Don't hesitate to reach out!</p>
+                            <h3 className="font-bold text-xl">Gotcha! Now you have to reach out. I don't make the rules.</h3>
+                            {/* <p className="flex justify-center">Don't hesitate to reach out!</p> */}
                             <div className="flex justify-center mt-10 flex-row">
                                 <button
                                     onClick={() =>
@@ -38,12 +34,22 @@ function App() {
                                     }
                                     className="flex items-center hover:text-gray-300"
                                 >
-                                    <img src={MailIcon} className="h-10 mx-5 fill-white" title='Will Open Your Default Email Client'/>
+                                    <img
+                                        src={MailIcon}
+                                        className="h-10 mx-5 fill-white"
+                                        title="Will Open Your Default Email Client"
+                                    />
                                 </button>
-                                <a href="https://www.linkedin.com/in/adamj7/" title="Link to My LinkedIn">
+                                <a
+                                    href="https://www.linkedin.com/in/adamj7/"
+                                    title="Link to My LinkedIn"
+                                >
                                     <img src={LinkedIn} className="h-10 mx-5" />
                                 </a>
-                                <a href="https://github.com/adaoj7" title="Link to My GitHub">
+                                <a
+                                    href="https://github.com/adaoj7"
+                                    title="Link to My GitHub"
+                                >
                                     <img src={Github} className="h-10 mx-5" />
                                 </a>
                             </div>
@@ -57,23 +63,23 @@ function App() {
                     </dialog>
                 </section>
             </header>
-            <main className="h-[2000px]">
-                <section className="flex flex-row justify-start h-1/4 bg-primary-content">
+            <main className="overflow-x-hidden">
+                <section className="flex flex-row justify-start h-screen bg-primary-content">
                     <section className="flex items-center justify-start ml-52">
                         <div>
-                            <div className="border-[15px] w-[350px] border-black rounded-2xl shadow-2xl">
-                                <img className="" src={Profile} />
+                            <div className=" w-[450px]">
+                                <img className="rounded-ss-[75px] rounded-se-[75px] rounded-es-[75px] rounded-ee-[75px]" src={Profile} />
                             </div>
                         </div>
                     </section>
                     <div className="flex w-full p-20 justify-center items-center mt-10">
                         <div className=" text-lg pr-20">
-                            <h2 className="text-white text-4xl font-Playfair font-semibold mb-6">
+                            <h2 className="text-white text-6xl font-Playfair font-semibold mb-6">
                                 Hi, I'm Adam
                             </h2>
                             <p className="text-white">
-                                One day, the semester before I
-                                graduated from college, while talking with my wife, a feeling
+                                One day, the semester before I graduated from
+                                college, while talking with my wife, a feeling
                                 hit me really strongly.{" "}
                                 <span className="font-bold text-info">
                                     "I should learn to code."{" "}
@@ -97,7 +103,9 @@ function App() {
                     </div>
                 </section>
                 <section className="flex justify-center flex-col items-center mt-4">
-                    <h2 className="mb-10 text-4xl font-Playfair font-semibold">My Projects</h2>
+                    <h2 className="my-10 text-6xl font-Playfair font-semibold">
+                        My Projects
+                    </h2>
                     <div className="card card-side w-2/3 bg-base-300 shadow-xl mt-10 mb-4">
                         {/* I need to take the screenshot from my large monitor */}
                         <figure className="w-2/3">
@@ -118,7 +126,10 @@ function App() {
                                 >
                                     Hosted Site
                                 </button>
-                                <a href="https://github.com/adaoj7/nativity_site" title='GitHub Repo'>
+                                <a
+                                    href="https://github.com/adaoj7/nativity_site"
+                                    title="GitHub Repo"
+                                >
                                     <img
                                         src={Github}
                                         alt="Github Logo"
@@ -138,8 +149,10 @@ function App() {
                                 If a dog chews shoes whose shoes does he choose?
                             </p>
                             <div className="card-actions justify-end flex flex-row">
-                                
-                                <a href="https://github.com/adaoj7/nativity_site" title='GitHub Repo'>
+                                <a
+                                    href="https://github.com/adaoj7/nativity_site"
+                                    title="GitHub Repo"
+                                >
                                     <img
                                         src={Github}
                                         alt="Github Logo"
@@ -159,8 +172,10 @@ function App() {
                                 If a dog chews shoes whose shoes does he choose?
                             </p>
                             <div className="card-actions justify-end flex flex-row">
-                                
-                                <a href="https://github.com/adaoj7/nativity_site" title='GitHub Repo'>
+                                <a
+                                    href="https://github.com/adaoj7/nativity_site"
+                                    title="GitHub Repo"
+                                >
                                     <img
                                         src={Github}
                                         alt="Github Logo"
@@ -172,7 +187,14 @@ function App() {
                     </div>
                 </section>
             </main>
-            <footer></footer>
+            <footer className="footer footer-center h-28 p-4 bg-base-300 text-base-content mt-8">
+                <aside className="text-base">
+                    <p>
+                        Copyright © 2024 - All right reserved by me, Adam Johnson
+                    </p>
+                    <p>Built w/ React, Tailwind, and DaisyUI</p>
+                </aside>
+            </footer>
         </div>
     );
 }
